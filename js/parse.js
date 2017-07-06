295096717b3c1eddf75005b0dc8ac5505151d049
undefinedlee <emianbao@vip.qq.com>
2017-07-06 20:10:58 +0800

    ...

--- js/parse.js
+++ js/parse.js
@@ -1,5 +1,18 @@
 var babel = require("babel-core");
 
 module.exports = function(code){
-	return babel.transform("function ______(){return " + code + "}").ast.program.body[0].body.body[0].argument;
+	return babel.transform(
+		"function ______(){return " + code + "}",
+		{
+			plugins: [
+				function () {
+					return {
+						manipulateOptions: function manipulateOptions(opts, parserOpts) {
+							parserOpts.plugins.push("*");
+						}
+					};
+				}
+			]
+		}
+	).ast.program.body[0].body.body[0].argument;
 };
