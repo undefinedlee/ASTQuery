1607c10230cda1679605dbcdeba363ef0a359432
undefinedlee <emianbao@vip.qq.com>
2017-07-07 16:37:02 +0800

    ...

--- js/stringify.js
+++ js/stringify.js
@@ -2,24 +2,32 @@ var babel = require("babel-core");
 
 module.exports = function(astNode){
 	var t = babel.types;
-	astNode = t.File(t.Program(
-		[
-			t.FunctionDeclaration(
-				t.Identifier("______"),
-				[],
-				t.BlockStatement([
-					t.ReturnStatement(astNode)
-				])
-			)
-		],
-		[]
-	), [], []);
-
-	return babel.transformFromAst(astNode).code
-				.replace(/^\s*function\s+_{6}\(\)\s*{/, "")
-				.replace(/}\s*$/, "")
-				.trim()
-				.replace(/^return\s+/, "")
-				.replace(/;$/, "")
-				.trim();
+	if(/(Expression|Literal)$/.test(astNode.type) || /^JSX/.test(astNode.type) || ["Identifier"].indexOf(astNode.type) !== -1){
+		// 转换表达式
+		astNode = t.File(t.Program(
+			[
+				t.FunctionDeclaration(
+					t.Identifier("______"),
+					[],
+					t.BlockStatement([
+						t.ReturnStatement(astNode)
+					])
+				)
+			],
+			[]
+		), [], []);
+		return babel.transformFromAst(astNode).code
+					.replace(/^\s*function\s+_{6}\(\)\s*{/, "")
+					.replace(/}\s*$/, "")
+					.trim()
+					.replace(/^return\s+/, "")
+					.replace(/;$/, "")
+					.trim();
+	}else{
+		astNode = t.File(t.Program(
+			[astNode],
+			[]
+		), [], []);
+		return babel.transformFromAst(astNode).code;
+	}
 };
