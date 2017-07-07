308caebcf392dc463a76bd66a912e30cd0c99e4c
undefinedlee <emianbao@vip.qq.com>
2017-07-07 17:53:41 +0800

    ...

--- js/stringify.js
+++ js/stringify.js
@@ -1,33 +1,27 @@
 var babel = require("babel-core");
 
+function isSimple(node){
+	var type = node.type;
+	return /(Expression|Literal)$/.test(type) || /^JSX/.test(type) || ["Identifier"].indexOf(type) !== -1;
+}
+
 module.exports = function(astNode){
 	var t = babel.types;
-	if(/(Expression|Literal)$/.test(astNode.type) || /^JSX/.test(astNode.type) || ["Identifier"].indexOf(astNode.type) !== -1){
-		// 转换表达式
-		astNode = t.File(t.Program(
-			[
-				t.FunctionDeclaration(
-					t.Identifier("______"),
-					[],
-					t.BlockStatement([
-						t.ReturnStatement(astNode)
-					])
-				)
-			],
-			[]
-		), [], []);
-		return babel.transformFromAst(astNode).code
-					.replace(/^\s*function\s+_{6}\(\)\s*{/, "")
-					.replace(/}\s*$/, "")
-					.trim()
-					.replace(/^return\s+/, "")
-					.replace(/;$/, "")
-					.trim();
-	}else{
-		astNode = t.File(t.Program(
-			[astNode],
-			[]
-		), [], []);
-		return babel.transformFromAst(astNode).code;
+	
+	if(!(astNode instanceof Array)){
+		astNode = [astNode];
 	}
+
+	astNode = astNode.map(function(node){
+		if(isSimple(node)){
+			return t.ExpressionStatement(node);
+		}
+		return node;
+	});
+
+	astNode = t.File(t.Program(
+		astNode,
+		[]
+	), [], []);
+	return babel.transformFromAst(astNode).code;
 };
